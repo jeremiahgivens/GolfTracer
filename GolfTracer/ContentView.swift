9d9bbeca18ef29b2c215557c7f4e7b919444f3b6
Jeremiah Givens <jeremiahgivens@Jeremiahs-Air.attlocal.net>
2023-11-01 20:38:40 -0500

    Initial commit.

---
+++ GolfTracer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GolfTracer
+//
+//  Created by Jeremiah Givens on 11/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GolfTracer/GolfTracerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfTracerApp.swift
+//  GolfTracer
+//
+//  Created by Jeremiah Givens on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct GolfTracerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
