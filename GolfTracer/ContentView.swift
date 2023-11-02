ed247a9bfc93eb8f282062f06a947be30c3b6da4
Jeremiah Givens <jeremiahgivens149@gmail.com>
2023-11-01 21:34:28 -0500

    Added mlcore model, added button for picking video, and added navigation link for viewing selected video.

--- GolfTracer/ContentView.swift
+++ GolfTracer/ContentView.swift
@@ -6,8 +6,36 @@
 //
 
 import SwiftUI
+import AVKit
+import PhotosUI
+
+struct Movie: Transferable {
+    let url: URL
+
+    static var transferRepresentation: some TransferRepresentation {
+        FileRepresentation(contentType: .movie) { movie in
+            SentTransferredFile(movie.url)
+        } importing: { received in
+            let copy = URL.documentsDirectory.appending(path: "movie.mp4")
+
+            if FileManager.default.fileExists(atPath: copy.path()) {
+                try FileManager.default.removeItem(at: copy)
+            }
+
+            try FileManager.default.copyItem(at: received.file, to: copy)
+            return Self.init(url: copy)
+        }
+    }
+}
 
 struct ContentView: View {
+    enum LoadState {
+        case unknown, loading, loaded(Movie), failed
+    }
+
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var loadState = LoadState.unknown
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -15,23 +43,49 @@ struct ContentView: View {
                     .ignoresSafeArea()
                 Form {
                     Section {
-                        Button("Select Video"){
-                            
-                        }
+                        PhotosPicker("Select Video", selection: $selectedItem, matching: .videos)
+                        switch loadState {
+                            case .unknown:
+                                EmptyView()
+                            case .loading:
+                                ProgressView()
+                            case .loaded(let movie):
+                                NavigationLink("View Video") {
+                                    VideoPlayer(player: AVPlayer(url: movie.url))
+                                        .scaledToFit()
+                                }
+                            case .failed:
+                                Text("Import failed")
+                            }
                     }
                     
                     Section {
                         NavigationLink("Crop Video") {
-                            Text("this is a new view")
+                            Text("View coming soon")
                         }
                         NavigationLink("Trace Time Range") {
-                            Text("this is a new view")
+                            Text("View coming soon")
                         }
                     }
                 }
                 .scrollContentBackground(.hidden)
             }
             .navigationTitle("Golf Tracer")
+        }
+        .onChange(of: selectedItem) {
+            Task {
+                do {
+                    loadState = .loading
+
+                    if let movie = try await selectedItem?.loadTransferable(type: Movie.self) {
+                        loadState = .loaded(movie)
+                    } else {
+                        loadState = .failed
+                    }
+                } catch {
+                    loadState = .failed
+                }
+            }
         }
     }
 }
