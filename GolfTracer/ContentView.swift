e3c961571500e45170354de459372fd8d3080612
Jeremiah Givens <jeremiahgivens@Jeremiahs-Air.attlocal.net>
2023-11-01 21:12:23 -0500

    Starting on initial UI layout.

--- GolfTracer/ContentView.swift
+++ GolfTracer/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                LinearGradient(colors: [.green, .black], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                Form {
+                    Section {
+                        Button("Select Video"){
+                            
+                        }
+                    }
+                    
+                    Section {
+                        NavigationLink("Crop Video") {
+                            Text("this is a new view")
+                        }
+                        NavigationLink("Trace Time Range") {
+                            Text("this is a new view")
+                        }
+                    }
+                }
+                .scrollContentBackground(.hidden)
+            }
+            .navigationTitle("Golf Tracer")
         }
-        .padding()
     }
 }
 
