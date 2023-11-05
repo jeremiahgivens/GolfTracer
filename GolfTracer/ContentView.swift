b9cf3bf723ea445a99391c73e6b85b8e3222c571
Jeremiah Givens <jeremiahgivens149@gmail.com>
2023-11-05 15:02:54 -0600

    Added tutorial code for adding animation layers over video.

--- GolfTracer/ContentView.swift
+++ GolfTracer/ContentView.swift
@@ -42,6 +42,8 @@ struct ContentView: View {
     @State private var selectedItem: PhotosPickerItem?
     @State private var loadState = LoadState.unknown
     @State private var movie: Movie?
+    @State private var isVideoAnalyzed = false
+    @State private var newVideoURL: URL?
     
     var body: some View {
         NavigationView {
@@ -93,6 +95,16 @@ struct ContentView: View {
                             }
                             case .failed:
                                 Text("Import failed")
+                        }
+                        if (isVideoAnalyzed){
+                            NavigationLink("View Analyzed Video") {
+                                ZStack {
+                                    Color.black
+                                        .ignoresSafeArea()
+                                    VideoPlayer(player: AVPlayer(url: newVideoURL!))
+                                        .frame(maxWidth: .infinity)
+                                }
+                            }
                         }
                     }
                 }
@@ -178,6 +190,8 @@ struct ContentView: View {
             reader.add(trackReaderOutput)
             reader.startReading()
             
+            CreateAnnotatedVideo(assetTrack: videoTrack[0], asset: asset)
+            
             do {
                 let model = try golfTracerModel()
                 
@@ -198,6 +212,204 @@ struct ContentView: View {
         }
     }
     
+    func CreateAnnotatedVideo(assetTrack: AVAssetTrack, asset: AVAsset) {
+        var composition = AVMutableComposition()
+        guard
+          let compositionTrack = composition.addMutableTrack(
+            withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)
+          else {
+            print("Something is wrong with the asset.")
+            return
+        }
+        
+        do {
+            let timeRange = CMTimeRange(start: .zero, duration: asset.duration)
+            try compositionTrack.insertTimeRange(timeRange, of: assetTrack, at: .zero)
+            
+            if let audioAssetTrack = asset.tracks(withMediaType: .audio).first,
+              let compositionAudioTrack = composition.addMutableTrack(
+                withMediaType: .audio,
+                preferredTrackID: kCMPersistentTrackID_Invalid) {
+              try compositionAudioTrack.insertTimeRange(
+                timeRange,
+                of: audioAssetTrack,
+                at: .zero)
+            }
+        } catch {
+            print(error)
+            return
+        }
+        
+        compositionTrack.preferredTransform = assetTrack.preferredTransform
+        let videoInfo = orientation(from: assetTrack.preferredTransform)
+        
+        let videoSize: CGSize
+        if videoInfo.isPortrait {
+          videoSize = CGSize(
+            width: assetTrack.naturalSize.height,
+            height: assetTrack.naturalSize.width)
+        } else {
+          videoSize = assetTrack.naturalSize
+        }
+        
+        let backgroundLayer = CALayer()
+        backgroundLayer.frame = CGRect(origin: .zero, size: videoSize)
+        let videoLayer = CALayer()
+        videoLayer.frame = CGRect(origin: .zero, size: videoSize)
+        let overlayLayer = CALayer()
+        overlayLayer.frame = CGRect(origin: .zero, size: videoSize)
+        
+        let outputLayer = CALayer()
+        outputLayer.frame = CGRect(origin: .zero, size: videoSize)
+        backgroundLayer.backgroundColor = UIColor(named: "rw-green")?.cgColor
+        videoLayer.frame = CGRect(
+          x: 20,
+          y: 20,
+          width: videoSize.width - 40,
+          height: videoSize.height - 40)
+        backgroundLayer.contents = UIImage(named: "background")?.cgImage
+        backgroundLayer.contentsGravity = .resizeAspectFill
+        addImage(to: overlayLayer, videoSize: videoSize)
+        add(
+          text: "Happy Birthday,\nJerble!",
+          to: overlayLayer,
+          videoSize: videoSize)
+        outputLayer.addSublayer(backgroundLayer)
+        outputLayer.addSublayer(videoLayer)
+        outputLayer.addSublayer(overlayLayer)
+        
+        let videoComposition = AVMutableVideoComposition()
+        videoComposition.renderSize = videoSize
+        videoComposition.frameDuration = CMTime(value: 1, timescale: 30)
+        videoComposition.animationTool = AVVideoCompositionCoreAnimationTool(
+          postProcessingAsVideoLayer: videoLayer,
+          in: outputLayer)
+        
+        let instruction = AVMutableVideoCompositionInstruction()
+        instruction.timeRange = CMTimeRange(
+          start: .zero,
+          duration: composition.duration)
+        videoComposition.instructions = [instruction]
+        let layerInstruction = compositionLayerInstruction(
+          for: compositionTrack,
+          assetTrack: assetTrack)
+        instruction.layerInstructions = [layerInstruction]
+        
+        guard let export = AVAssetExportSession(
+          asset: composition,
+          presetName: AVAssetExportPresetHighestQuality)
+          else {
+            print("Cannot create export session.")
+            return
+        }
+        
+        let videoName = UUID().uuidString
+        let exportURL = URL(fileURLWithPath: NSTemporaryDirectory())
+          .appendingPathComponent(videoName)
+          .appendingPathExtension("mov")
+
+        export.videoComposition = videoComposition
+        export.outputFileType = .mov
+        export.outputURL = exportURL
+        
+        export.exportAsynchronously {
+          DispatchQueue.main.async {
+            switch export.status {
+            case .completed:
+                //onComplete(exportURL)
+                // Call your method to display the video
+                newVideoURL = exportURL
+                isVideoAnalyzed = true
+                break
+            default:
+              print("Something went wrong during export.")
+              print(export.error ?? "unknown error")
+              break
+            }
+          }
+        }
+    }
+    
+    private func compositionLayerInstruction(for track: AVCompositionTrack, assetTrack: AVAssetTrack) -> AVMutableVideoCompositionLayerInstruction {
+      let instruction = AVMutableVideoCompositionLayerInstruction(assetTrack: track)
+      let transform = assetTrack.preferredTransform
+      
+      instruction.setTransform(transform, at: .zero)
+      
+      return instruction
+    }
+    
+    private func addImage(to layer: CALayer, videoSize: CGSize) {
+      let image = UIImage(named: "overlay")!
+      let imageLayer = CALayer()
+        let aspect: CGFloat = image.size.width / image.size.height
+        let width = videoSize.width
+        let height = width / aspect
+        imageLayer.frame = CGRect(
+          x: 0,
+          y: -height * 0.15,
+          width: width,
+          height: height)
+        imageLayer.contents = image.cgImage
+        layer.addSublayer(imageLayer)
+    }
+
+    
+    private func orientation(from transform: CGAffineTransform) -> (orientation: UIImage.Orientation, isPortrait: Bool) {
+      var assetOrientation = UIImage.Orientation.up
+      var isPortrait = false
+      if transform.a == 0 && transform.b == 1.0 && transform.c == -1.0 && transform.d == 0 {
+        assetOrientation = .right
+        isPortrait = true
+      } else if transform.a == 0 && transform.b == -1.0 && transform.c == 1.0 && transform.d == 0 {
+        assetOrientation = .left
+        isPortrait = true
+      } else if transform.a == 1.0 && transform.b == 0 && transform.c == 0 && transform.d == 1.0 {
+        assetOrientation = .up
+      } else if transform.a == -1.0 && transform.b == 0 && transform.c == 0 && transform.d == -1.0 {
+        assetOrientation = .down
+      }
+      
+      return (assetOrientation, isPortrait)
+    }
+    
+    private func add(text: String, to layer: CALayer, videoSize: CGSize) {
+      let attributedText = NSAttributedString(
+        string: text,
+        attributes: [
+          .font: UIFont(name: "ArialRoundedMTBold", size: 60) as Any,
+          .foregroundColor: UIColor(named: "rw-green")!,
+          .strokeColor: UIColor.white,
+          .strokeWidth: -3])
+      
+      let textLayer = CATextLayer()
+      textLayer.string = attributedText
+      textLayer.shouldRasterize = true
+      textLayer.rasterizationScale = UIScreen.main.scale
+      textLayer.backgroundColor = UIColor.clear.cgColor
+      textLayer.alignmentMode = .center
+      
+      textLayer.frame = CGRect(
+        x: 0,
+        y: videoSize.height * 0.66,
+        width: videoSize.width,
+        height: 150)
+      textLayer.displayIfNeeded()
+      
+      let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+      scaleAnimation.fromValue = 0.8
+      scaleAnimation.toValue = 1.2
+      scaleAnimation.duration = 0.5
+      scaleAnimation.repeatCount = .greatestFiniteMagnitude
+      scaleAnimation.autoreverses = true
+      scaleAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
+      
+      scaleAnimation.beginTime = AVCoreAnimationBeginTimeAtZero
+      scaleAnimation.isRemovedOnCompletion = false
+      textLayer.add(scaleAnimation, forKey: "scale")
+      
+      layer.addSublayer(textLayer)
+    }
 }
 
 #Preview {
